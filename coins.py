bf322a68306e7beb9cc77c6e3e7c16bd7efec778
Paul Marillonnet <pmarillonnet@entrouvert.com>
2017-07-18 16:16:45 +0200

    Coins change (HR) : XXX only 1st solution returned

--- coins.py
+++ coins.py
@@ -1,31 +1,29 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 import sys
-from numpy import cross
-
-def print_change(coins, n):
-    make_change(coins, n, n)
 
 
-def make_change(coins, n):
-    quantities = [0 for _ in coins]
-    if n == 0: return quantities
+
+def make_change(coins, num):
+    if num == 0: return [0 for _ in coins]
 
     for coin in coins:
-        if coin <= n:
+        if coin <= num:
+            quantities = [0 for _ in coins]
+            updated_quantities = []
             quantities[coins.index(coin)] += 1
-            subquantities = make_changes(coins, n - coin)
+            subquantities = make_change(coins, num - coin)
             if not subquantities:
                 continue
             for i in range(len(quantities)):
-                quantities[i] += subquantities[i]
-            if n == ntot and sum(cross(quantities,coins)) == ntot:
-                res_list.append(quantities)
-            return quantities
-    
+                updated_quantities.append(quantities[i] + subquantities[i])
+            if num == ntot and sum([q*c for q, c in zip(updated_quantities,coins)]) == ntot:
+                res_list.append(updated_quantities)
+            return updated_quantities # XXX yield?
+
 res_list = []
 n,m = input().strip().split(' ')
 ntot,m = [int(n),int(m)]
 coins = [int(coins_temp) for coins_temp in input().strip().split(' ')]
-make_change(coins, n)
-
+make_change(coins, ntot)
+print(len(res_list))
